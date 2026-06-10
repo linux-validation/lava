f327a4db8e9ec04342e4f9169062eb514b68eb70
Milosz Wasilewski <milosz.wasilewski@oss.qualcomm.com>
2026-06-10 14:14:00 +0100

    actions: detect qdl version in boot-to-qdl
    When trying to flash boards with qdl, some features are only available
    above certain versions of the tool. For example --skipblock is only
    available on v2.7 and above. This patch checks the installed version and
    adds options accordingly.
    
    Signed-off-by: Milosz Wasilewski <milosz.wasilewski@oss.qualcomm.com>

--- tests/lava_dispatcher/test_qdl.py
+++ tests/lava_dispatcher/test_qdl.py
@@ -6,13 +6,16 @@
 
 from unittest.mock import patch
 
+from lava_common.exceptions import ConfigurationError
 from tests.lava_dispatcher.test_basic import Factory, LavaDispatcherTestCase
 
 
 class TestQDLBootAction(LavaDispatcherTestCase):
+    @patch("lava_dispatcher.action.Action.parsed_command")
     @patch("lava_dispatcher.actions.boot.qdl.which")
-    def test_qdl_job(self, which_mock):
+    def test_qdl_job(self, which_mock, parsed_mock):
         which_mock.return_value = "/foo/qdl"
+        parsed_mock.return_value = "qdl version v2.7"
         job = Factory().create_job("qcs6490-rb3gen2", "sample_jobs/qdl-boot.yaml")
         job.device.update({"board_qdl_id": "abcdef12"})
         job.device.update({"board_id": "abcdef12"})
@@ -23,3 +26,25 @@ class TestQDLBootAction(LavaDispatcherTestCase):
             self.assertTrue(action.valid)
         description_ref = self.pipeline_reference("qdl.yaml", job=job)
         self.assertEqual(description_ref, job.pipeline.describe())
+
+    @patch("lava_dispatcher.actions.boot.qdl.which")
+    def test_qdl_job_no_qdl(self, which_mock):
+        which_mock.return_value = ""
+        job = Factory().create_job("qcs6490-rb3gen2", "sample_jobs/qdl-boot.yaml")
+        job.device.update({"board_qdl_id": "abcdef12"})
+        job.device.update({"board_id": "abcdef12"})
+        self.assertEqual(len(job.pipeline.actions), 4)
+        with self.assertRaises(ConfigurationError):
+            job.validate()
+
+    @patch("lava_dispatcher.action.Action.parsed_command")
+    @patch("lava_dispatcher.actions.boot.qdl.which")
+    def test_qdl_job_qdl_1(self, which_mock, parsed_mock):
+        which_mock.return_value = "/foo/qdl"
+        parsed_mock.return_value = "qdl version v1.0"
+        job = Factory().create_job("qcs6490-rb3gen2", "sample_jobs/qdl-boot.yaml")
+        job.device.update({"board_qdl_id": "abcdef12"})
+        job.device.update({"board_id": "abcdef12"})
+        self.assertEqual(len(job.pipeline.actions), 4)
+        with self.assertRaises(ConfigurationError):
+            job.validate()
